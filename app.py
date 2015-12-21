0ee95518b7c25cb83bfd30aeb038012f0dc87cd2
Amy <amyyuexiao@gmail.com>
2015-12-20 20:13:32 -0500

    migrations

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import *
+from models import Result
+
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
-	return "Hello {}!".format(name)
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
-	print(os.environ['APP_SETTINGS'])
-	app.run()
+    app.run()
